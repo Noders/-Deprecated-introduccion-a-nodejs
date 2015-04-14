c7e86c07c8466d7a02a3eb6e70c74af163f43f88
fforres mac <felipe.torressepulveda@gmail.com>
2015-04-14 10:37:41 -0300

    agregando indentacion correcta a los códigos

--- clase 2/code/express/NodersApp/routes/index.js
+++ clase 2/code/express/NodersApp/routes/index.js
@@ -3,19 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', 
-    
-
-
-    {
+    res.render('index', {
         title: 'Express'
-    }
-
-
-    );
+    });
 });
 router.get('/pcmasterrace', function(req, res, next) {
-	var unirest = require('unirest');
+    var unirest = require('unirest');
     unirest.get("https://byroredux-metacritic.p.mashape.com/game-list/pc/coming-soon")
         .header("X-Mashape-Key", "mgJXDeuxhUmshog39jEw2pJSRkQDp1vNOBWjsnzDkSjYxIxnvl")
         .header("Accept", "application/json")
