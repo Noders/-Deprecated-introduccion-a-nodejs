83b90948c643462fb0edd96d36a45c2d7c20906b
fforres <felipe.torressepulveda@gmail.com>
2015-04-27 04:39:40 -0300

    template clase 4

---
+++ clase 4/code/nodersapp/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+router.get('/pcmasterrace', function(req, res, next) {
+    var unirest = require('unirest');
+    unirest.get("https://byroredux-metacritic.p.mashape.com/game-list/pc/coming-soon")
+        .header("X-Mashape-Key", "mgJXDeuxhUmshog39jEw2pJSRkQDp1vNOBWjsnzDkSjYxIxnvl")
+        .header("Accept", "application/json")
+        .end(function(result) {
+            res.render('game', {
+                title: 'Express',
+                juegos: result.body.results
+            });
+        });
+});
+
+
+
+module.exports = router;
---
+++ clase 4/code/nodersapp/routes/perros.js
@@ -0,0 +1,125 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var Perro = mongoose.model('Perro', {
+    nombre: String
+});
+
+mongoose.connect('mongodb://localhost/nodersapp_if');
+
+
+/* GET lista de Perros. */
+router.get('/', function(req, res, next) {
+    Perro.find(function(err, perros) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(perros);
+            res.render('perros/lista',
+
+                {
+                    title: 'Mis Perros',
+                    perros: perros
+                }
+
+
+
+            );
+        }
+    });
+});
+
+
+
+
+/* Formulario para Crear Perros. */
+router.get('/crear', function(req, res, next) {
+    res.render('perros/crear', {
+        title: 'Creando un Perro'
+    });
+});
+
+/* POST para Crear Perros. */
+router.post('/crear', function(req, res, next) {
+
+    console.log(req.body)
+    console.log(req.params)
+    var unPerro = Perro({
+        nombre: req.body.nombre
+    });
+
+
+    unPerro.save(function(err, elPerro) {
+        if (err) {
+            console.log(err);
+            res.send("Hay un error")
+        } else {
+            res.redirect("/perros");
+        }
+    });
+
+
+
+
+});
+
+
+
+
+/* GET un perro. */
+router.get('/:id', function(req, res, next) {
+
+
+    Perro.findById(req.params.id, function(err, perro) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(perro);
+            res.render('perros/unosolo', {
+                title: 'Mi Perro',
+                perro: perro
+            });
+        }
+    });
+
+
+});
+
+
+
+
+
+
+/* DELETE un perro. */
+router.get('/:id/borrar', function(req, res, next) {
+    Perro.findOneAndRemove(
+
+        {
+            _id: req.params.id
+        },
+        function(err, perro) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(perro);
+                res.redirect("/perros")
+            }
+        });
+});
+
+
+router.get('/crearperroalamala', function(req, res, next) {
+    var unPerro = Perro({
+        nombre: "cachupin: " + Date.now()
+    });
+    unPerro.save(function(err, elPerro) {
+        if (err)
+            console.log(err);
+        else
+            console.log(elPerro);
+        res.send('Perro Creado :)');
+    });
+});
+
+module.exports = router;
